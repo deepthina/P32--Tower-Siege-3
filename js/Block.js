5ac02183553b8d41e8993872a641612fd45d6a50
deepthina <deepthi.an.87@gmail.com>
2021-02-04 19:29:11 +0530

    p32

---
+++ js/Block.js
@@ -0,0 +1,44 @@
+class Block {
+    constructor(x, y) {
+
+        var options = {
+            restitution: 0.1,
+            friction: 0.4,
+            density: 0.5
+        }
+
+        this.body = Bodies.rectangle(x, y, 50, 60, options);
+        this.width = 50;
+        this.height = 60;
+        this.visibility = 255;
+
+        World.add(world, this.body);
+    }
+
+
+    display() {
+
+        if (this.body.speed < 3) {
+            var pos = this.body.position;
+
+            rectMode(CENTER);
+            rect(pos.x, pos.y, this.width, this.height);
+        }
+
+        else {
+            var pos = this.body.position;
+            World.remove(world, this.body);
+            push();
+            this.visibility= this.visibility-1;
+            console.log(this.visibility)
+            tint(255, this.visibility);            
+            pop();
+        }
+    }
+
+    score(){
+        if(this.visibility>200 && this.visibility<230 ){
+            score++;
+        }
+    }
+}
